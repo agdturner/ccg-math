d6f49a7b67d4b9ce625c6075343a6d2e700c7457
agdtu <agdtu@DESKTOP-S5UTMD4>
2020-11-27 07:30:16 +0000

    Null edit.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module uk.ac.leeds.ccg.math {
     requires transitive java.logging;
     requires java.desktop;
     requires ch.obermuhlner.math.big;
-    requires uk.ac.leeds.ccg.generic;
+    requires transitive uk.ac.leeds.ccg.generic;
     //requires uk.ac.leeds.ccg.generic.core;
     //requires uk.ac.leeds.ccg.generic.util;
     //requires uk.ac.leeds.ccg.generic.io;
