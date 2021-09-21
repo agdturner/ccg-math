941673bba991a0db219711581e287be26bef12c9
agdtu <agdtu@DESKTOP-S5UTMD4>
2021-09-21 10:48:40 +0100

    Release.

--- src/test/java/uk/ac/leeds/ccg/math/Math_BigRationalSqrtTest.java
+++ src/test/java/uk/ac/leeds/ccg/math/Math_BigRationalSqrtTest.java
@@ -136,7 +136,7 @@ public class Math_BigRationalSqrtTest {
         int oomi = -1;
         Math_BigRationalSqrt instance = new Math_BigRationalSqrt(2, oomi);
         String expResult = "Math_BigRationalSqrt(x=2, negative=false, "
-                + "sqrtx=null, oomi=-1, sqrtxapprox=null, oom=0)";
+                + "sqrtx=null, oomi=-1, sqrtxapprox=1.4, oom=0)";
         String result = instance.toString();
         assertEquals(expResult, result);
         // Test 2
